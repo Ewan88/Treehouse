e7660f9619048047acda59ba86120124e159ebae
Ewan <baileyewan@gmail.com>
2023-05-26 23:45:49 +0100

    guestlist

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 use std::io::stdin;
 
+fn what_is_your_name() -> String {
+    let mut name = String::new();
+    stdin().read_line(&mut name).expect("Failed to read line");
+    name.trim().to_lowercase()
+}
+
 fn main() {
     println!("Hello, what's your name?");
-    let mut your_name = String::new();
-    stdin()
-        .read_line(&mut your_name)
-        .expect("Failed to read line");
-    println!("Hello, {}", your_name)
+    let name = what_is_your_name();
+    println!("Hello, {:?}", name);
+
+    let visitor_list = ["bert", "steve", "fred"];
+
+    let mut allow_them_in = false;
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true;
+        }
+    }
+
+    if allow_them_in {
+        println!("Welcome to the Treehouse, {}", name);
+    } else {
+        println!("Sorry, you aren't on the list.");
+    }
 }
